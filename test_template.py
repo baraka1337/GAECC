620f907b9f3e2133a99ea12314dcd0c5bb28126c
Barak Aharoni <1999baraka@gmail.com.com>
2024-05-23 17:50:15 +0300

    now test different n, k, ebn0

--- test_template.py
+++ test_template.py
@@ -266,6 +266,29 @@ def test_13():
     }
     test_template(param)
 
+def test_all():
+    param = {
+        "k": 24,
+        "n": 49,
+        "num_initial_population": 1000,
+        "sample_size": 1000,
+        "p_mutation": 0.01,
+        "num_generations": 50,
+        "ebn0": 4,
+        "num_parents_mating": 100,
+        "offspring_size": 800,
+        "delta": 1e-20,
+        "gamma": 7,
+    }
+    n_k = [(32, 11), (64, 32), (31, 16), (63, 35), (49, 24), (60, 52)]
+    ebn0 = [4, 5, 6]
+    for e in ebn0:
+        for n, k in n_k:
+            param["k"] = k
+            param["n"] = n
+            param["ebn0"] = e
+            print(f"Running test for k={param['k']}, n={param['n']}, ebn0={param['ebn0']}")
+            test_template(param)
 
 def test_template(param):
     name = "_".join(f"{a}_{b}" for a, b in param.items())
@@ -309,13 +332,4 @@ def plot_all_generations(ga, name):
 
 
 if __name__ == "__main__":
-    print("Test 9:")
-    test_9()
-    print("Test 10:")
-    test_10()
-    print("Test 11:")
-    test_11()
-    print("Test 12:")
-    test_12()
-    print("Test 13:")
-    test_13()
+    test_all()
